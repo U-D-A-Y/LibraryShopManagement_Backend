ebb2c491c7786666b69f830c41aa115d14f5a835
DevUday <udayusq@gmail.com>
2020-10-05 23:44:27 +0600

    working on transaction api

--- api/libraryApi.js
+++ api/libraryApi.js
@@ -5,7 +5,7 @@ const database = require('../databaseConfi')
 const getData = query => {
     return new Promise((resolve, reject) => {
         database.connection.query(query, (err, result, field) => {
-           resolve(result);
+            resolve(result);
         })
     })
 }
@@ -13,13 +13,13 @@ const getData = query => {
 
 // Categories Api
 
-    router.get('/categories',(req,res)=>{
-        let query="select * from categorys";
-        getData(query)
-        .then(result=>{
+router.get('/categories', (req, res) => {
+    let query = "select * from categorys";
+    getData(query)
+        .then(result => {
             res.json(result)
         })
-    })
+})
 
 
 
@@ -39,47 +39,88 @@ router.get('/books', (req, res) => {
     join publishers as p 
     on b.publication_by_fk=p.publisher_pk;`
     getData(query)
-    .then(result=>{
+        .then(result => {
             res.json(result)
-    })
+        })
 
 })
 
-router.post('/book',(req,res)=>{
-    
-    let query=`INSERT INTO books (NAME,ISBN,CATEGORY_FK,AUTHOR_NAME,PUBLICATION_BY_FK,PUBLICATION_YEAR,NUMBER_OF_PAGE,PRICE,QUANTITY,DESCRIPTION)values("${req.body.BOOK_NAME}","${req.body.ISBN}",${req.body.CATEGORY_FK},"${req.body.AUTHOR_NAME}",${req.body.PUBLICATION_BY_FK}, ${req.body.PUBLICATION_YEAR}, ${req.body.NUMBER_OF_PAGE},${req.body.PRICE}, ${req.body.QUANTITY}, "${req.body.DESCRIPTION}");
+router.post('/book', (req, res) => {
+
+    let query = `INSERT INTO books (NAME,ISBN,CATEGORY_FK,AUTHOR_NAME,PUBLICATION_BY_FK,PUBLICATION_YEAR,NUMBER_OF_PAGE,PRICE,QUANTITY,DESCRIPTION)values("${req.body.BOOK_NAME}","${req.body.ISBN}",${req.body.CATEGORY_FK},"${req.body.AUTHOR_NAME}",${req.body.PUBLICATION_BY_FK}, ${req.body.PUBLICATION_YEAR}, ${req.body.NUMBER_OF_PAGE},${req.body.PRICE}, ${req.body.QUANTITY}, "${req.body.DESCRIPTION}");
     `
     console.log(query)
     database.connection.query(query, (err, result, field) => {
-        if(result){
+        if (result) {
             res.json(result)
         }
-        if(err)
+        if (err)
             res.json(err)
-     
-     })
-   
+
+    })
+
+
+
 })
-router.delete('/book/:id',(req,res)=>{
-    
-    let query=`DELETE FROM books where BOOK_PK=${req.params.id}`
+router.delete('/book/:id', (req, res) => {
+
+    let query = `DELETE FROM books where BOOK_PK=${req.params.id}`
     console.log(query)
     database.connection.query(query, (err, result, field) => {
-        if(result){
+        if (result) {
             res.json(result)
         }
-        if(err)
+        if (err)
             res.json(err)
-     
-     })
-   
+
+    })
+
 })
 //Publisher API
 router.get('/publishers', (req, res) => {
     let query = `select * from publishers`
     getData(query)
-    .then(result=>{
+        .then(result => {
             res.json(result)
+        })
+
+})
+
+
+
+// customer
+
+router.get('/customers', (req, res) => {
+    let query = `select * from customers`
+    getData(query)
+        .then(result => {
+            res.json(result)
+        })
+})
+
+
+
+// sales 
+router.post('/transaction', (req, res) => {
+    database.connection.beginTransaction((err) => {
+        if (err) { res.json(err) }
+        let query = `INSERT IGNORE  INTO customers (name,phone,customer_type,mail,address) values ("${req.body.customer.NAME}","${req.body.customer.PHONE}",1,"${req.body.customer.MAIL}","${req.body.customer.ADDRESS}") ;`
+        database.connection.query(query, (err, result, field) => {
+            if (err) {
+                database.connection.rollback(() => {
+                    res.json(err);
+                })
+            }
+            query = `select customer_pk from  customers where phone="${req.body.customer.PHONE}";`
+            database.connection.query(query, (err, result, field) => {
+                if (err) {
+                    database.connection.rollback(() => {
+                        res.json(err);
+                    })
+                }
+
+            })
+        })
     })
 
 })
